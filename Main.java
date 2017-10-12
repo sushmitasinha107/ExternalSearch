c62b8a361db5a1e7fc1a260a94db37c38d17a92c
sushmitasinha107 <sushmitasinha107@gmail.com>
2017-10-11 21:54:15 -0400

    Hashmap approach

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Main {
+	 private static final int N = 30;
+
+	public  static void main(String args[]) throws IOException {
+		// TODO Auto-generated constructor stub
+		 BufferedWriter fbw = new BufferedWriter(new FileWriter("result123.txt"));
+		 Map<Integer, HashSet<String>> map = new HashMap<Integer, HashSet<String>>();
+		 BufferedReader fbr = new BufferedReader(new FileReader("access2.log"));
+		 String  line = "";
+		
+		 while((line = fbr.readLine()) != null){
+	        String values[] = line.split(",");		     
+			int key = Integer.parseInt(values[1]);
+			HashSet<String> setItems =  new HashSet<String> ();
+			if (map.containsKey(key)) {
+				setItems = map.get(key);
+				setItems.add( values[2]);
+				map.put(key, setItems);
+			} else {
+				setItems.add(values[2]);
+				map.put(key, setItems);
+			} 
+		}
+		 
+		Set<Integer> resultSet = new TreeSet<Integer>();
+	 	HashSet<String> set = new HashSet<String>();
+		Iterator<Entry<Integer, HashSet<String>>> iterator = map.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Entry<Integer, HashSet<String>> entry = (Entry<Integer, HashSet<String>>) iterator.next();
+			
+			set = (HashSet<String>) entry.getValue();
+			if((Integer)entry.getKey() == 16460 ){System.out.println(set);}
+			if(set.size()>= N){
+				resultSet.add((Integer) entry.getKey());
+			}
+		}
+		
+			
+		for(Integer tempRes : resultSet){
+			fbw.write( Integer.toString(tempRes));
+			fbw.write('\n');
+		 }
+		fbw.close();
+
+		 
+	}
+}
